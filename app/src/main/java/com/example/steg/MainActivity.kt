2c9bf97ed0621a32a5f3fdbf7d50c1b7d2e15c29
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-07-14 23:51:07 +0900

    viewBinding 설정

--- app/src/main/java/com/example/steg/MainActivity.kt
+++ app/src/main/java/com/example/steg/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.steg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.steg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
