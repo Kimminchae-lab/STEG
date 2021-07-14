703c7f05aa8826fe7336043ab52b23cbbf607499
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-07-14 15:13:51 +0900

    화면 전환

--- app/src/main/java/com/example/steg/SplashActivity.kt
+++ app/src/main/java/com/example/steg/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.example.steg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
